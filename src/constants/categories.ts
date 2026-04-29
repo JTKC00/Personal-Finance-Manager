3cdd3af1d1c506fce97643357e032121718ba90a
JamesTong <kachuntong01@gmail.com>
2026-04-29 22:44:09 +0800

    Expand income categories

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -1,4 +1,4 @@
 export const expenseCategories = ['餐飲', '交通', '購物', '娛樂', '醫療', '居住', '金融支出', '學習', '禮物', '旅遊', '保險', '家庭', '其他'];
-export const incomeCategories = ['薪資', '投資', '副業', '其他收入'];
+export const incomeCategories = ['薪資', '獎金', '投資', '利息', '副業', '自由工作', '生意收入', '租金收入', '退款', '回贈', '報銷', '禮金', '政府津貼', '其他收入'];
 
 export const paymentMethods = ['信用卡', '現金', '電子錢包'];
